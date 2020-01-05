fbbb17df489ee9f3b40643aff94eb25ccab97781
limiteds233 <ss2333_1@mail.ru>
2020-01-05 10:03:01 +0300

    add valid

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
   self.abstract_class = true
 end
